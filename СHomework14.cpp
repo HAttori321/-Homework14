799b584e20596865ada94f312953842cc9d86861
Nazar <Nazar@COMPUTER>
2024-04-07 14:51:13 +0300

    Add project files.

---
+++ СHomework14.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Fraction {
+	int num;
+	int den;
+
+	Fraction add(const Fraction& other) const
+	{
+		Fraction result;
+		result.num = num * other.den + other.num * den;
+		result.den = den * other.den;
+		return result;
+	}
+	Fraction subtract(const Fraction& other) const
+	{
+		Fraction result;
+		result.num = num * other.den - other.num * den;
+		result.den = den * other.den;
+		return result;
+	}
+	Fraction multiply(const Fraction& other) const
+	{
+		Fraction result;
+		result.num = num * other.num;
+		result.den = den * other.den;
+		return result;
+	}
+	Fraction divide(const Fraction& other) const
+	{
+		Fraction result;
+		result.num = num * other.den;
+		result.den = den * other.num;
+		return result;
+	}
+	void print() const
+	{
+		cout << num << "/" << den;
+	}
+};
+
+int main()
+{
+	
+	Fraction A = { 6, 9 };
+	Fraction B = {26, 21};
+	
+    cout << "A + B = ";
+	A.add(B).print();
+    cout << endl;
+	
+    cout << "A - B = ";
+    A.subtract(B).print();
+    cout << endl;
+	
+    cout << "A * B = ";
+    A.multiply(B).print();
+    cout << endl;
+	
+    cout << "A / B = ";
+    A.divide(B).print();
+    cout << endl;
+}
